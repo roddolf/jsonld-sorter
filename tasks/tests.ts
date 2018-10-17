92fc976f7b7e23d29d25ea9ef1f012fc589d70fb
Rodolfo Aguirre <rodolfo.aguirre@base22.com>
2018-10-16 20:37:03 -0500

    Release v0.1.0

--- tasks/tests.ts
+++ tasks/tests.ts
@@ -1,3 +1,18 @@
+import sequence from "@start/plugin-sequence";
 import jest from "@start/plugin-lib-jest";
+import find from "@start/plugin-find";
+import read from "@start/plugin-read";
+import coveralls from "./plugins/coveralls";
 
 export const test = () => jest({});
+
+export const ci = () =>
+    sequence(
+        jest({
+            collectCoverage: true,
+            coverageReporters: ["lcov"],
+        }),
+        find("coverage/lcov.info"),
+        read,
+        coveralls(),
+    );
