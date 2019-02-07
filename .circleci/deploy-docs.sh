af1327edc81319d7b82921c0cc4d98e7e277d144
Rodolfo Aguirre <aguirreg.rodolfo@gmail.com>
2019-02-07 09:56:54 -0600

    Fix printing of steps

--- .circleci/deploy-docs.sh
+++ .circleci/deploy-docs.sh
@@ -11,26 +11,26 @@ git config --global user.name "$GH_NAME"
 git clone --single-branch --branch gh-pages $CIRCLE_REPOSITORY_URL gh-pages
 
 # Clean GitHub Pages
-echo "\nClean gh-pages directory"
+echo -e "\nClean gh-pages directory"
 cd gh-pages
-git rm -rfv .
+git rm -rf .
 
 # Copy docs to GitHub Pages
-echo "\nCopy docs into gh-pages directory"
+echo -e "\nCopy docs into gh-pages directory"
 cd ..
-cp -av docs/. gh-pages/.
+cp -av docs/* gh-pages
 # Copy Circle CI config
 mkdir -p gh-pages/.circleci
 cp -av .circleci/config.yml gh-pages/.circleci/config.yml
 
 # Publish new GitHub Pages
-echo "\nDeploy gh-pages branch"
+echo -e "\nDeploy gh-pages branch"
 cd gh-pages
 git add -A
 git commit -m "Circle CI deplayment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
 git push origin gh-pages
 
 # Clean GitHub Pages directory
-echo "\nClean gh-pages directory"
+echo -e "\nClean gh-pages directory"
 cd ..
 rm -rfv gh-pages
