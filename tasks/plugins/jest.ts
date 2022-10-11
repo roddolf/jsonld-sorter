707b9ae5e648896b90c1ab2b7b4b0e77d61c20e4
= <=>
2022-10-10 22:39:28 -0700

    Create own jest start plugin

---
+++ tasks/plugins/jest.ts
@@ -0,0 +1,28 @@
+import { Config } from '@jest/types'
+import plugin from '@start/plugin'
+import { Arguments } from 'yargs';
+
+export type JestArgv = Config.Argv extends Arguments<infer U> ? U : never;
+const JEST_ARGS_REQUIRED: Arguments = {
+    _: [],
+    $0: "jest",
+};
+
+export default (argv?: JestArgv) =>
+    plugin('jest', () => async () => {
+        const { runCLI } = await import('jest');
+        const projects = argv?.projects ?? [argv?.rootDir ?? process.cwd()];
+
+        const result = await runCLI({
+            ...JEST_ARGS_REQUIRED,
+            ...argv,
+        }, projects);
+
+        if (
+            result.results.numFailedTests > 0 ||
+            result.results.numFailedTestSuites > 0 ||
+            result.results.numTotalTests === 0
+        ) {
+            throw null;
+        }
+    });
