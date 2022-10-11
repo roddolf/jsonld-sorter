a53106551c17fa38f8fc21aad1c0783c78a4521b
= <=>
2022-10-11 00:51:08 -0700

    Fix microbundle and dist files

--- tasks/plugins/microbundle.ts
+++ tasks/plugins/microbundle.ts
@@ -5,15 +5,21 @@ export default (options: {} = {}) =>
         // @ts-ignore
         const microbundle = (await import("microbundle")).default;
         try {
-            await microbundle({
-                format: "es,cjs,umd",
+            const { output } = await microbundle({
+                entries: [],
+                format: 'modern,esm,cjs,umd',
+                // format: "esm,cjs,umd",
                 watch: false,
+                workers: false,
+                'pkg-main': true,
                 target: "node",
-                compress: true,
+                compress: false,
                 cwd: ".",
                 sourcemap: true,
                 ...options,
             });
+            utils.logMessage(output);
+
         } catch (e) {
             throw e instanceof Error
                 ? e.message
